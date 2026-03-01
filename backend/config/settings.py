a0757de810481bf47432e6e6be9275ae92f4b4d6
lokesh3964 <plokesh3964@gmail.com>
2026-03-01 22:01:43 +0530

    Added gunicorn and updated requirements

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -71,7 +71,7 @@ if os.environ.get('MYSQL_DATABASE'):
             'USER': os.environ.get('MYSQL_USER'),
             'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
             'HOST': os.environ.get('MYSQL_HOST'),
-            'PORT': os.environ.get('MYSQL_PORT', '3306'),
+            'PORT': os.environ.get('MYSQL_PORT'),
             'OPTIONS': {
                 'charset': 'utf8mb4',
             },
